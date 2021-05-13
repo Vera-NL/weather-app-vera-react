efee3ec6a9560ad6690ffa2f3d7839c27d23696d
Vera-NL <78375434+Vera-NL@users.noreply.github.com>
2021-05-13 17:05:24 +0200

    Use of flexbox in forecast-hours

--- src/ForecastHours.js
+++ src/ForecastHours.js
@@ -2,25 +2,28 @@ import React from "react";
 
 
 export default function ForecastHours() {
-    return (<div className="row">
-                <div className="col-sm-2"><strong>17.00</strong><br /> 
-                <i class="fas fa-sun fa-2x" ></i> <br />
-                9° 4°</div>
-                <div className="col-sm-2"><strong>20.00</strong><br /> 
-                <i class="fas fa-sun fa-2x"></i><br />
-                9° 4° </div>
-                <div className="col-sm-2"><strong>23.00</strong><br /> 
-                <i class="fas fa-sun fa-2x"></i><br />
-                9° 4° </div>
-                <div className="col-sm-2"><strong>02.00</strong><br /> 
-                <i class="fas fa-sun fa-2x"></i><br />
-                9° 4° </div>
-                <div className="col-sm-2"><strong>05.00</strong><br /> 
-                <i class="fas fa-sun fa-2x"></i><br />
-                9° 4° </div>
-                <div className="col-sm-2"><strong>08.00</strong><br /> 
-                <i class="fas fa-sun fa-2x"></i><br />
-                9° 4° </div>
+    return (
+            <div className="container-fluid d-flex">
+                <div className="row">
+                    <div className="col-sm-2"><strong>17.00</strong><br /> 
+                    <i class="fas fa-sun fa-2x" ></i> <br />
+                    9° 4°</div>
+                    <div className="col-sm-2"><strong>20.00</strong><br /> 
+                    <i class="fas fa-sun fa-2x"></i><br />
+                    9° 4° </div>
+                    <div className="col-sm-2"><strong>23.00</strong><br /> 
+                    <i class="fas fa-sun fa-2x"></i><br />
+                    9° 4° </div>
+                    <div className="col-sm-2"><strong>02.00</strong><br /> 
+                    <i class="fas fa-sun fa-2x"></i><br />
+                    9° 4° </div>
+                    <div className="col-sm-2"><strong>05.00</strong><br /> 
+                    <i class="fas fa-sun fa-2x"></i><br />
+                    9° 4° </div>
+                    <div className="col-sm-2"><strong>08.00</strong><br /> 
+                    <i class="fas fa-sun fa-2x"></i><br />
+                    9° 4° </div>
+                </div>
             </div>
         );
     }
