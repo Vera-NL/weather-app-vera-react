175ec874c8472906eb5ff26cf7f5b09077f8181f
Vera-NL <78375434+Vera-NL@users.noreply.github.com>
2021-05-21 12:55:27 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import Loader from "react-loader-spinner";
 import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
-// https://ferg-weather-react.netlify.app/
 
 export default function Weather(props) {
   let [weatherData, setWeatherData] =  useState({ready: false});
